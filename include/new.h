1d3dbc2ea65042216c0bb301384ff1845e6d2a35
Mohamed fareed <131618380+mfc0d1ng@users.noreply.github.com>
2025-07-26 17:21:54 +0300

    Update new.h

--- include/new.h
+++ include/new.h
@@ -1,3 +1,15 @@
+/**
+ * A set of macro function for safer memory allocation C.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * 
+ * @file new.h
+ * 
+ * Written by Mohamed fareed
+ **/
+
 #ifndef NEW_H
 #define NEW_H
 
@@ -8,8 +20,8 @@
  * 
  * User is responsible for handling the returned pointer.
  */
-#define unsafe_new_object(T) ({   \
-    (T *) calloc(1, sizeof(T));   \
+#define unsafe_new(T) ({  \
+    calloc(1, sizeof(T)); \
 })
 
 /**
@@ -17,8 +29,8 @@
  * @param T Data type.
  * @return A pointer to the allocated memory.
  */
-#define new_object(T) ({                \
-    T* __ptr = calloc(1, sizeof(T));    \
+#define new(T) ({                       \
+    void* __ptr = calloc(1, sizeof(T)); \
     if(!__ptr){ exit(EXIT_FAILURE); }   \
     __ptr;                              \
 })
@@ -29,8 +41,8 @@
  * @param ERR Error message.
  * @return A pointer to the allocated memory.
  */
-#define new_object_(T, ERR) ({          \
-    T* __ptr = calloc(1, sizeof(T));    \
+#define _new(T, ERR) ({                 \
+    void* __ptr = calloc(1, sizeof(T)); \
     if(!__ptr) {                        \
         fprintf(stderr, "%s\n", ERR);   \
         exit(EXIT_FAILURE);             \
@@ -41,23 +53,23 @@
 /**
  * @brief Allocates a block of memory for N elements of type T.
  * @param T Data type.
- * @param N Number of elements.
+ * @param N Number of elements required.
  * @return A pointer to the allocated memory.
  * 
  * User is responsible for handling the returned pointer.
  */
-#define unsafe_new(T, N) ({        \
-    (T *) calloc(N, sizeof(T));    \
+#define unsafe_new_block(T, N) ({ \
+     calloc(N, sizeof(T));        \
 })
 
 /**
  * @brief Allocates a block of memory for N elements of type T.
  * @param T Data type.
- * @param N Number of elements.
+ * @param N Number of elements required.
  * @return A pointer to the allocated memory.
  */
-#define new(T, N) ({                    \
-    T* __ptr = calloc(N, sizeof(T));    \
+#define new_block(T, N) ({              \
+    void* __ptr = calloc(N, sizeof(T)); \
     if(!__ptr){ exit(EXIT_FAILURE); }   \
     __ptr;                              \
 })
@@ -65,12 +77,12 @@
 /**
  * @brief Allocates a block of memory for N elements of type T.
  * @param T Data type.
- * @param N Number of elements.
+ * @param N Number of elements required.
  * @param ERR Error message.
  * @return A pointer to the allocated memory.
  */
-#define new_(T, N, ERR) ({              \
-    T* __ptr = calloc(N, sizeof(T));    \
+#define _new_block(T, N, ERR) ({        \
+    void* __ptr = calloc(N, sizeof(T)); \
     if(!__ptr) {                        \
         fprintf(stderr, "%s\n", ERR);   \
         exit(EXIT_FAILURE);             \
@@ -84,9 +96,9 @@
  * 
  * This function free the memory and set @a PTR to NULL.
  */
-#define delete(PTR) ({    \
-    free(PTR);            \
-    PTR = NULL;           \
+#define delete(PTR) ({ \
+    free(PTR);         \
+    PTR = NULL;        \
 })
 
 #endif
